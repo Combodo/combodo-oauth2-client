01dd515fcd217cf3a63050c52cf0592431c08200
odain <olivier.dain@combodo.com>
2024-11-13 17:26:40 +0100

    test GetToken live with real oauth github token

---
+++ tests/php-unit-tests/unitary-tests/Oauth2ClientServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Combodo\iTop\MFATotp\Test;
+
+use Combodo\iTop\Oauth2Client\Service\Oauth2ClientService;
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+use GithubOauth2Client;
+use Combodo\iTop\Application\Helper\Session;
+use MetaModel;
+
+/**
+ * @runClassInSeparateProcess Required because PHPUnit outputs something earlier, thus causing the headers to be sent
+ */
+class Oauth2ClientServiceTest extends ItopDataTestCase
+{
+	const USE_TRANSACTION = false;
+
+	protected function setUp(): void {
+		parent::setUp();
+		Session::$bAllowCLI = true;
+
+		$this->RequireOnceItopFile('env-production/combodo-oauth2-client/vendor/autoload.php');
+	}
+
+	protected function tearDown(): void
+	{
+		parent::tearDown();
+		Session::$bAllowCLI = false;
+	}
+
+	public function testGetToken() {
+		//session_start();
+		//Session::$bAllowCLI = false;
+		$this->assertNull(Session::$iSessionId);
+		Session::Start();
+		$this->assertNotNull(Session::$iSessionId);
+		$_SERVER['REMOTE_ADDR'] = '1.2.3.4';
+
+		$oGithubOauth2Client = MetaModel::GetObject(GithubOauth2Client::class, 4);
+		$sToken = $oGithubOauth2Client->Get('access_token');
+		$this->assertEquals($sToken, Oauth2ClientService::GetInstance()->GetToken($oGithubOauth2Client));
+	}
+}
