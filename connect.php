5d58f4645fbcb96a8ae2c141d8554a01716b5213
odain <olivier.dain@combodo.com>
2024-12-12 17:20:52 +0100

    fix connect during authorize

--- connect.php
+++ connect.php
@@ -27,6 +27,7 @@ try {
 	$sProvider = base64_decode(\utils::ReadParam('provider'));
 	$sAction = \utils::ReadParam('action');
 	Session::Set('oauth2_client_name', $sName);
+	Session::Set('oauth2_client_provider', $sProvider);
 	Session::Set('transaction_id', \utils::GetNewTransactionId());
 
 	Oauth2Service::GetInstance()->Init($sName, $sProvider);
