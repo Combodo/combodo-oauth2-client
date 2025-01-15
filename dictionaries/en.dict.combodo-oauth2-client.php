416d2a76e9fb759f5912cfeedd259d95ae46f954
odain <olivier.dain@combodo.com>
2025-01-15 16:56:44 +0100

    N°8066 - Polishing Webhook Oauth

--- dictionaries/en.dict.combodo-oauth2-client.php
+++ dictionaries/en.dict.combodo-oauth2-client.php
@@ -23,4 +23,35 @@ Dict::Add('EN US', 'English', 'English', [
 
 	'Menu:Oauth2Client' => 'OAuth 2.0 client',
 	'Menu:Oauth2Client+' => 'General purpose OAuth 2.0 Client',
+
+	'Class:Oauth2Client' => 'OAuth 2.0 client',
+	'Class:Oauth2Client/Attribute:name' => 'Name',
+	'Class:Oauth2Client/Attribute:provider' => 'Provider',
+	'Class:Oauth2Client/Attribute:client_id' => 'Client ID',
+	'Class:Oauth2Client/Attribute:client_secret' => 'Client Secret',
+	'Class:Oauth2Client/Attribute:refresh_token' => 'Refresh Token',
+	'Class:Oauth2Client/Attribute:access_token' => 'Access Token',
+	'Class:Oauth2Client/Attribute:refresh_token_expiration' => 'Refresh Token Expiration',
+	'Class:Oauth2Client/Attribute:access_token_expiration' => 'Access Token Expiration',
+	'Class:Oauth2Client/Attribute:scope' => 'Scope',
+	'Class:Oauth2Client/Attribute:authorization_state' => 'Authorization State',
+	'Class:Oauth2Client/Attribute:status' => 'Status',
+	'Class:Oauth2Client/Attribute:token_type' => 'Token Type',
+
+	'Class:GitHubOauth2Client' => 'GitHub OAuth 2.0 client',
+
+	'Class:MicrosoftGraphOauth2Client' => 'Microsoft Graph OAuth 2.0 client',
+	'Class:MicrosoftGraphOauth2Client/Attribute:tenant' => 'Tenant',
+
+	'Class:GoogleOauth2Client' => 'Google OAuth 2.0 client',
+
+	'Class:HeadlessItopOauth2Client' => 'Headless OAuth 2.0 client',
+	'Class:HeadlessItopOauth2Client/Attribute:username' => 'Username',
+	'Class:HeadlessItopOauth2Client/Attribute:password' => 'Password',
+	'Class:HeadlessItopOauth2Client/Attribute:base_url' => 'Base URL',
+	'Class:HeadlessItopOauth2Client/Attribute:version' => 'Version',
+
+	'Class:KeycloakOauth2Client' => 'Keycloak OAuth 2.0 client',
+	'Class:KeycloakOauth2Client/Attribute:url' => 'URL',
+	'Class:KeycloakOauth2Client/Attribute:realm' => 'Realm',
 ]);
--- dictionaries/fr.dict.combodo-oauth2-client.php
+++ dictionaries/fr.dict.combodo-oauth2-client.php
@@ -23,4 +23,35 @@ Dict::Add('FR FR', 'French', 'Français', [
 
 	'Menu:Oauth2Client' => 'Client OAuth 2.0',
 	'Menu:Oauth2Client+' => 'Client OAuth 2.0 générique',
+
+	'Class:Oauth2Client' => 'Client OAuth 2.0',
+	'Class:Oauth2Client/Attribute:name' => 'Nom',
+	'Class:Oauth2Client/Attribute:provider' => 'Fournisseur',
+	'Class:Oauth2Client/Attribute:client_id' => 'ID du Client',
+	'Class:Oauth2Client/Attribute:client_secret' => 'Secret du Client',
+	'Class:Oauth2Client/Attribute:refresh_token' => 'Jeton de rafraîchissement',
+	'Class:Oauth2Client/Attribute:access_token' => 'Jeton d\'accès',
+	'Class:Oauth2Client/Attribute:refresh_token_expiration' => 'Expiration du Jeton de rafraîchissement',
+	'Class:Oauth2Client/Attribute:access_token_expiration' => 'Expiration du Jeton d\'accès',
+	'Class:Oauth2Client/Attribute:scope' => 'Scope',
+	'Class:Oauth2Client/Attribute:authorization_state' => 'Statut d\'Autorisation State',
+	'Class:Oauth2Client/Attribute:status' => 'Statut',
+	'Class:Oauth2Client/Attribute:token_type' => 'Type de Token',
+
+	'Class:GitHubOauth2Client' => 'Client OAuth 2.0 GitHub',
+
+	'Class:MicrosoftGraphOauth2Client' => 'Client OAuth 2.0 Microsoft Graph',
+	'Class:MicrosoftGraphOauth2Client/Attribute:tenant' => 'Tenant',
+
+	'Class:GoogleOauth2Client' => 'Client OAuth 2.0 Google',
+
+	'Class:HeadlessItopOauth2Client' => 'Client OAuth 2.0 Headless',
+	'Class:HeadlessItopOauth2Client/Attribute:username' => 'Nom d\'utilisateur',
+	'Class:HeadlessItopOauth2Client/Attribute:password' => 'Mot de Passe',
+	'Class:HeadlessItopOauth2Client/Attribute:base_url' => 'URL de Base',
+	'Class:HeadlessItopOauth2Client/Attribute:version' => 'Version',
+
+	'Class:KeycloakOauth2Client' => 'Client OAuth 2.0 Keycloak',
+	'Class:KeycloakOauth2Client/Attribute:url' => 'URL',
+	'Class:KeycloakOauth2Client/Attribute:realm' => 'Realm',
 ]);
