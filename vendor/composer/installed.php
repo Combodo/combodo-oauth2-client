f35ed6cad5052f3104d98eea6e5b6431c101ebc3
odain <olivier.dain@combodo.com>
2026-05-12 15:10:15 +0200

    N°9576 - User provisioning fail and cannot use whole Keycloak GetUser response

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'combodo/combodo-oauth2-client',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1eaea9c830f99bf5bd5d655bc6f080fca25f9199',
         'type' => 'itop-extension',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,23 +11,21 @@
     ),
     'versions' => array(
         'combodo/combodo-oauth2-client' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1eaea9c830f99bf5bd5d655bc6f080fca25f9199',
             'type' => 'itop-extension',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'hybridauth/hybridauth' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'da3cd2132dead3078b61dba80ae2c45226b53dd6',
+            'pretty_version' => '3.12.1.1',
+            'version' => '3.12.1.1',
+            'reference' => 'e71cbf7fd76310f9a3953a19302722309425655a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../hybridauth/hybridauth',
-            'aliases' => array(
-                0 => '9999999-dev',
-            ),
+            'aliases' => array(),
             'dev_requirement' => false,
         ),
     ),
